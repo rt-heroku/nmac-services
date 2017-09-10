655d8c5555328467be3a03e3aa8e9714085c098a
X817001 <X817001@USLH250613.NMCorp.Nissan.Biz>
2017-09-09 22:48:44 -0500

    Options try

--- src/main/java/com/nmac/payments/controllers/NmacUserRestController.java
+++ src/main/java/com/nmac/payments/controllers/NmacUserRestController.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -221,5 +223,13 @@ public class NmacUserRestController {
 	    return e.getMessage();
 	}
 
+	 @RequestMapping(
+	            value = "/**",
+	            method = RequestMethod.OPTIONS
+	    )
+	    public ResponseEntity handle() {
+	        return new ResponseEntity(HttpStatus.OK);
+	    }
+	
 }
 
