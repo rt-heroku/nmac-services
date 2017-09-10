7cfc6272ba2018f7975718470abab692298685fa
X817001 <X817001@USLH250613.NMCorp.Nissan.Biz>
2017-09-10 00:42:32 -0500

    CORS Try

--- src/main/java/com/nmac/payments/CORSConfig.java
+++ src/main/java/com/nmac/payments/CORSConfig.java
@@ -1,15 +1,18 @@
 package com.nmac.payments;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CORSConfig {
 
-    @Bean
+  /*  @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
@@ -22,4 +25,18 @@ public class CORSConfig {
             }
         };
     }
+    */
+	
+	 @Bean
+	    public FilterRegistrationBean corsFilter() {
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        CorsConfiguration config = new CorsConfiguration();
+	        		config.setAllowedOrigins(Arrays.asList("*"));
+	        config.setAllowedMethods(Arrays.asList("*"));
+	        config.setAllowCredentials(true);
+	        source.registerCorsConfiguration("/**", config);
+	        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	        bean.setOrder(0);
+	        return bean;
+	    }
 }
